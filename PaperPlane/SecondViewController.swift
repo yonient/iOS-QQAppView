63baf252ff69f746fbfb4740ade139421bda7363
Kenneth Zhang <smallbearayu@icloud.com>
2016-04-11 19:20:41 +0800

    Initial Commit

---
+++ PaperPlane/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PaperPlane
+//
+//  Created by Kenneth Zhang on 16/4/11.
+//  Copyright © 2016年 Kenneth Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
